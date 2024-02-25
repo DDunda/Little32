72fb761d4f9f02f3d9b1337f657495ffd4ac5386
Dunda <50138952+DDunda@users.noreply.github.com>
2024-02-26 10:58:41 +1100

    Better keyboard.asm and keyboard.cfg

--- Little32/scripts/keyboard.asm
+++ Little32/scripts/keyboard.asm
@@ -1,67 +1,63 @@
-$clocks_per_frame = 27 // Enough to call on_down a single time
+$clocks_per_frame = 26 // Enough to call on_down a single time
 $frame_delay = 125 // 8 FPS
 
 ${:{
 
-$keyboard = R4
-$char_mem = R5
-$key      = R6
+$char_mem = R4
+$key      = R5
+$mask     = R6
 
+#ROM FORCE
 #ENTRY
-MOV $char_mem, CHAR_MEM
+MOV $mask, 0xF
 
-MOV $keyboard, keyboard
-RRW $keyboard, [$keyboard]
 MOV R0, on_down
-RWW R0, [$keyboard]
+RWW R0, [.KEYBOARD]
 
 RRW R0, [.zeroes]
-RWW R0, [$char_mem]
-RWW R0, [$char_mem+4]
+RWW R0, [.CHAR_MEM]
+RWW R0, PC, .CHAR_MEM+4 // [.CHAR_MEM+4]
 HALT
 
-zeroes: 0x30303030
-
 on_down:
-	RRW $key, [$keyboard+8]
+	RRW $key, PC, .KEYBOARD+8 // [.KEYBOARD+8]
 
-	MOV R1, 15
-
-	AND R0, R1, $key ROTR 28
+	AND R0, $mask, $key ROTR 28
 	RRB R0, [R0+chars]
-	RWB R0, [$char_mem]
+	RWB R0, [.CHAR_MEM]
 
-	AND R0, R1, $key ROTR 24
+	AND R0, $mask, $key ROTR 24
 	RRB R0, [R0+chars]
-	RWB R0, [$char_mem+1]
+	RWB R0, PC, .CHAR_MEM+1 // [.CHAR_MEM+1]
 	
-	AND R0, R1, $key ROTR 20
+	AND R0, $mask, $key ROTR 20
 	RRB R0, [R0+chars]
-	RWB R0, [$char_mem+2]
+	RWB R0, PC, .CHAR_MEM+2 // [.CHAR_MEM+2]
 
-	AND R0, R1, $key ROTR 16
+	AND R0, $mask, $key ROTR 16
 	RRB R0, [R0+chars]
-	RWB R0, [$char_mem+3]
+	RWB R0, PC, .CHAR_MEM+3 // [.CHAR_MEM+3]
 
-	AND R0, R1, $key ROTR 12
+	AND R0, $mask, $key ROTR 12
 	RRB R0, [R0+chars]
-	RWB R0, [$char_mem+4]
+	RWB R0, PC, .CHAR_MEM+4 // [.CHAR_MEM+4]
 
-	AND R0, R1, $key ROTR 8
+	AND R0, $mask, $key ROTR 8
 	RRB R0, [R0+chars]
-	RWB R0, [$char_mem+5]
+	RWB R0, PC, .CHAR_MEM+5 // [.CHAR_MEM+5]
 
-	AND R0, R1, $key ROTR 4
+	AND R0, $mask, $key ROTR 4
 	RRB R0, [R0+chars]
-	RWB R0, [$char_mem+6]
+	RWB R0, PC, .CHAR_MEM+6 // [.CHAR_MEM+6]
 
-	AND R0, R1, $key ROTR 0
+	AND R0, $mask, $key ROTR 0
 	RRB R0, [R0+chars]
-	RWB R0, [$char_mem+7]
+	RWB R0, PC, .CHAR_MEM+7 // [.CHAR_MEM+7]
 
 	RFE
 
 #DATA
+zeroes: 0x30303030
 keyboard: KEYBOARD
 
 #BYTE
